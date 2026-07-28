3d8a58075a12a9dc41ca33470887ddfda625c7f8
fabianamartinsconsulting-code <fabianamartins.consulting@gmail.com>
2026-07-27 23:05:37 -0300

    Create supabase-config.js

---
+++ js/supabase-config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: "https://hewjallqxbugwloxrbyq.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhld2phbGxxeGJ1Z3dsb3hyYnlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxOTI3NjIsImV4cCI6MjEwMDc2ODc2Mn0.5YfthWtz6EP0xKfbeLJkV0E4ODSQZU8v-6z4v7BMMhU",
+};
